b5b007d1b2214b2e9ea9a1c7d5c39942afba03a2
Alan Iwi <alan.iwi@stfc.ac.uk>
2018-09-13 07:05:45 +0100

    Crontab changes:
    1) change to last 8 days instead of 5, so can be put in cron.weekly with a slight overlap
    
    2) change check_replicas.py command line to avoid '--date=' option because it doesn't interact well with --dry-run at end of line
    
    3) write output to a log file

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 # Example script that can be run as a crontab to check local CMIP5 replicas
-# that have changed in the last 5 days
+# that have changed in the last 8 days
+# Symlink into /etc/cron.weekly
 
 export CDAT_HOME=/usr/local/conda
 source ${CDAT_HOME}/bin/activate esgf-pub
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH=${SOURCE_DIR}
-python ${SOURCE_DIR}/esgfpy/update/check_replicas.py CMIP5 `date +%Y-%m-%d --date='5 days ago'`
+
+logfile=/esg/log/check_replicas_cron.log
+date >> $logfile
+
+# example with --dry-run
+#python ${SOURCE_DIR}/esgfpy/update/check_replicas.py CMIP5 $(date -d '8 days ago' +%Y-%m-%d) $(date +%Y-%m-%d) --dry-run
+
+python ${SOURCE_DIR}/esgfpy/update/check_replicas.py CMIP5 $(date -d '8 days ago' +%Y-%m-%d) $(date +%Y-%m-%d) >> $logfile 2>&1
