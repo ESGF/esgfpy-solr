3752b286a319437f60e9250ee5415f5640ed5164
Guillaume <glipsl@ipsl.fr>
2019-02-05 17:55:23 +0100

    Update

--- esgfpy/update/check_replicas.py
+++ esgfpy/update/check_replicas.py
@@ -17,7 +17,7 @@ local_master_solr_url = 'http://localhost:8984/solr'
 
 # any ESGF index node used to retrieve the full list
 # of the index nodes in the federation
-esgf_index_node_url = 'https://esgf-node.llnl.gov/esg-search/search/'
+esgf_index_node_url = 'https://esgf-node.ipsl.upmc.fr/esg-search/search/'
 
 # by default, the script will check data that changed in the last N days
 LAST_NUMBER_OF_DAYS = 7
@@ -30,15 +30,16 @@ def check_replicas(project,
                    stop_datetime=datetime.datetime.strftime(
                        datetime.datetime.now(), '%Y-%m-%dT%H:%M:%SZ'),
                    dry_run=False):
-    '''
+    """
     Checks replicas for a specific project.
     By default it will check datasets that have changed in the past week.
     start_datetime, stop_datetime must be string in the format
     "2017-01-07T00:00:00.831Z".
-    '''
-
-    logging.info("Checking replicas start datetime=%s stop datetime=%s "
-                 "dry_run=%s" % (start_datetime, stop_datetime, dry_run))
+    """
+    msg = 'Checking replicas published from {} to {}'.format(start_datetime, stop_datetime)
+    if dry_run:
+        msg += ' :: DRY RUN ::'
+    logging.info(msg)
 
     # 0) retrieve the latest list of ESGF index nodes
     # query: https://esgf-node.jpl.nasa.gov/esg-search/search/?offset=0
@@ -53,81 +54,70 @@ def check_replicas(project,
     # "index_node":["esg-dn1.nsc.liu.se", 78954, "esg.pik-potsdam.de",66899,
     #               "esgdata.gfdl.noaa.gov",5780,...]
     index_nodes = jobj['facet_counts']['facet_fields']['index_node'][0::2]
-    logging.debug("Querying index nodes: %s" % index_nodes)
+    logging.debug('Querying index nodes: {}'.format(index_nodes))
 
     # counter
     num_datasets_updated = 0
 
-    # 1) query all remote index nodes for the latest primary datasets
-    # that have changed in the given time period
+    # Fields to query to Solr
     fields = ['id', 'master_id', 'version', '_timestamp']
+
+    # 1) query local index for replicas list
+    # that are flagged with latest=True
+    logging.debug('Get local replicas from: {}'.format(local_master_solr_url))
+    query = ('project:%s&replica:true&latest:true' % (project))
+    replicas = query_solr(query, fields, solr_url=local_master_solr_url, solr_core='datasets')
+    replicas_ids = [i['master_id'] for i in replicas]
+
+    # Assert the list of replicas flagged as latest is unique
+    assert len(replicas_ids) == len(set(replicas_ids))
+
+    # 2) query all remote index nodes for the latest primary datasets
+    # that have changed in the given time period
+    logging.debug('Starting retrieval of primaries dataset')
     for index_node in index_nodes:
-
+        remote_slave_solr_url = 'https://{}/solr'.format(index_node)
         try:
-
-            remote_slave_solr_url = 'https://%s/solr' % index_node
-            logging.info("Querying Solr=%s for datasets with project=%s "
-                         "start_datetime=%s stop_datetime=%s" % (
-                             remote_slave_solr_url, project,
-                             start_datetime, stop_datetime))
-            query1 = ('project:%s&replica:false&latest:true'
+            msg = 'Querying {} for {} datasets published from {} to {}'.format(remote_slave_solr_url,
+                                                                               project,
+                                                                               start_datetime,
+                                                                               stop_datetime)
+            logging.info(msg)
+            query = ('project:%s&replica:false&latest:true'
                       '&_timestamp:[%s TO %s]' % (
                           project, start_datetime, stop_datetime))
-            docs1 = query_solr(query1, fields, solr_url=remote_slave_solr_url,
-                               solr_core='datasets')
-            if len(docs1) > 0:
-                logging.info("\tFound %s datasets that have changed, "
-                             "checking local Solr for replicas" % len(docs1))
-
+            primaries = query_solr(query, fields, solr_url=remote_slave_solr_url, solr_core='datasets')
         except urllib.error.HTTPError:
-            logging.error("Error querying index node "
-                          "%s" % remote_slave_solr_url)
-            docs1 = []
+            logging.error('Error querying {}'.format(remote_slave_solr_url))
+            primaries = []
 
-        # 2) query local index for replicas of the same datasets
-        # that are flagged with latest='true'
-        for doc1 in docs1:
-            v1 = int(doc1['version'])
-            master_id = doc1['master_id']
-            dataset_id1 = doc1['id']
-            _timestamp1 = doc1['_timestamp']
-            logging.info("\tChecking local Solr=%s for replica of dataset=%s "
-                         "version=%s _timestamp=%s" % (
-                             local_master_solr_url, dataset_id1, v1,
-                             _timestamp1))
+        primaries_ids = [(i['master_id'], i['version']) for i in primaries]
+        # Assert the list of primaries flagged as latest is unique
+        assert len(primaries_ids) == len(set(primaries_ids))
 
-            query2 = 'master_id:%s&replica:true&latest:true' % master_id
-            docs2 = query_solr(query2, fields, solr_url=local_master_solr_url,
-                               solr_core='datasets')
+        # iterate over common datasets between replicas and primaries
+        logging.debug('Compare local replicas with primaries from {}'.format(remote_slave_solr_url))
+        for d in set(zip(*replicas_ids)[0]).intersection(zip(*primaries_ids)[0]):
+            replicas_versions = sorted([int(r['version']) for r in replicas if r['master_id'] == d])
+            primaries_versions = sorted([int(p['version']) for p in primaries if p['master_id'] == d])
 
-            # check local 'latest' replica
-            for doc2 in docs2:
-
-                # compare versions
-                v2 = int(doc2['version'])
-                # master_id2 = doc2['master_id']
-                dataset_id2 = doc2['id']
-
-                # remote primary has newer version -->
-                # local replica must be updated
-                if v1 > v2:
-                    logging.warn("\t\tFound newer version: %s for dataset: %s "
-                                 "at site: %s" % (
-                                     v2, master_id, remote_slave_solr_url))
-                    logging.warn("\t\tUpdating status of local dataset: %s to "
-                                 "latest=false" % dataset_id2)
-
-                    # 3) set latest flag of local replica to false
-                    # for datasets, files, aggregations
+            # If version history is different between local replicas and primaries
+            if replicas_versions != primaries_versions:
+                # Compare latest version
+                if replicas_versions[-1] < primaries_versions[-1]:
+                    msg = 'Found newer latest version {} for dataset {} at site {}'.format(primaries_versions[-1],
+                                                                                           d,
+                                                                                           remote_slave_solr_url)
+                    logging.warn(msg)
+                    # Update solr latest flag metadata
+                    # 3) set latest flag of local replica to false for datasets, files, aggregations
                     if not dry_run:
-                        update_dict = {'id:%s' % dataset_id2: {
-                            'latest': ['false']}}
+                        update_dict = {'id:%s' % d: {'latest': ['false']}}
                         update_solr(update_dict,
                                     update='set',
                                     solr_url=local_master_solr_url,
                                     solr_core='datasets')
-                        update_dict = {'dataset_id:%s' % dataset_id2: {
-                            'latest': ['false']}}
+                        update_dict = {'dataset_id:%s' % d: {'latest': ['false']}}
                         update_solr(update_dict,
                                     update='set',
                                     solr_url=local_master_solr_url,
@@ -136,12 +126,15 @@ def check_replicas(project,
                                     update='set',
                                     solr_url=local_master_solr_url,
                                     solr_core='aggregations')
-
+                    # increase counter
                     num_datasets_updated += 1
+                else:
+                    logging.warn("\t\tFound older latest master version: %s for dataset: %s at site: %s" % (
+                        max(replicas_versions), d, remote_slave_solr_url))
+                    # TODO: Trigger unpublication ??
 
-    logging.info("Total number of local replica updated="
-                 "%s" % num_datasets_updated)
-
+    msg = 'Total number of local replicas updated: {}'.format(num_datasets_updated)
+    logging.info(msg)
 
 if __name__ == '__main__':
 
